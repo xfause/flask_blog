f2eef06c7d26cc3902d253f2c2eec94ea5fcdcb8
xfause <919017553@qq.com>
2017-05-25 20:10:04 +0800

    commit-16
    1. fix upload problem of picture in forPic.py
    2. simplify some html code

--- forPic.py
+++ forPic.py
@@ -7,12 +7,12 @@ import os
 
 def getToken():
 	# 七牛 密钥
-	AK = os.environ.get('AK') or '' #access key
-	SK = os.environ.get('SK') or '' #secret key
-	d1 = int(time.mktime(datetime.datetime.now().timetuple()))+21600
-	s = '{"scope":"BUCKET","deadline":%s}'%(dl,) #BUCKET填自己存储空间bucket的名字
-	s = base64.urlsafe_b64encode(s)
-	sign = hmac.new(SK,s,sha1).digest()
+	AK = os.environ.get('AK') or 'UkIkqJcV3DvInPK0zXsHaZBD6UYjLGaRGzLybY7J' #access key
+	SK = os.environ.get('SK') or 'WfyHKYFtijCvX03i6pTUjqwUeFjwUM2CYy5jXZuT' #secret key
+	dl = int(time.mktime(datetime.datetime.now().timetuple()))+21600
+	s = '{"scope":"blog-pic","deadline":%s}'%(dl,) #BUCKET填自己存储空间bucket的名字
+	s = base64.urlsafe_b64encode(bytes(s,'utf-8'))
+	sign = hmac.new(b'SK',s,sha1).digest()
 	sign = base64.urlsafe_b64encode(sign)
-	token = AK + ':' + sign + ':' + s
+	token = AK + ':' + str(sign) + ':' + str(s)
 	return token
